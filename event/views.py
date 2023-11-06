e13f80d75187f88fc0ebffc234e82e97d29387fc
gentlesoul18 <oyebamijibarakat7@gmail.com>
2023-11-06 10:27:30 +0100

    adding

--- event/views.py
+++ event/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+
+from event.serializers import EventSerializer
 # Create your views here.
+@api_view(['POST'])
+def create_event(request):
+    data = request.data
+    
